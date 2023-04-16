d1ce6a1e3e6acfda211197f977a31631e7e7cd8e
Hanna S <ann.shchur666@gmail.com>
2023-04-16 23:06:53 +0300

    connecting app to bd

--- src/main/java/com/awesome/blade/BladeApplication.java
+++ src/main/java/com/awesome/blade/BladeApplication.java
@@ -3,11 +3,13 @@ package com.awesome.blade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class BladeApplication {
 
 	public static void main(String[] args) {
+		var timestamp = LocalDateTime.now();
 		SpringApplication.run(BladeApplication.class, args);
 	}
-
 }
